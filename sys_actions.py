0b3f8ce1689a4965efebafd1456bb19d91328612
davidoberst <juandavidbustosperez82@gmail.com>
2026-07-31 00:43:19 -0500

    delete file action

--- sys_actions.py
+++ sys_actions.py
@@ -32,4 +32,24 @@ def create_file(name: str, contenido: str = "") -> dict:
     except Exception as e:
         return {"exito": False, "error": str(e)}
 
+
+def delete_files(): 
+    joined_path = os.path.join(PATH, name)
+    ruta_resuelta = os.path.abspath(joined_path)  
+    if not ruta_resuelta.startswith(os.path.abspath(PATH) + os.sep):
+        return {"exito": False, "error": "ruta fuera del espacio de trabajo permitido"}
+    try :
+        if os.path.exists(ruta_resuelta):
+         os.remove(ruta_resuelta)
+         print("[!] Archivo eliminado")
+         return {"exito": True, "ruta": os.path.abspath(ruta_resuelta)}
+        else: 
+         print("[X] El archivo no existe.")
+    except : 
+        return {"exito": False, "error": str(e)}
+
+
+    
+
+
  
